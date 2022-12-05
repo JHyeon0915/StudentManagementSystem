2ae1276a5e38a31c95f625602cd8325230e5ec2a
Jeonghyeon Lee <jeonghyeon@Jeonghyeons-MacBook-Pro.local>
2022-12-04 16:44:23 -0800

    Fix inserting of valiable data problem

--- StudentManagementSystem.py
+++ StudentManagementSystem.py
@@ -4,37 +4,24 @@ class CS22B:
         self.prof = prof
         self.head = data[0].strip().split(",")
         self.data = data[1:]
-        print(self.data)
+
         for line in self.data:             # future concern: no function to adjust along the column name
             line = line.strip().split(",")
-            print(line)
-            self.students.append(Student(line[0], line[1], int(line[2]), int(line[3]), int(line[4]), int(line[5]), int(line[6]), line[7], int(line[8])))
-        for s in self.students:
-            print(s.getName())
-            print(s.getGrade('teamproject'))
-
-    def rewrite(self):
-        self.data = []
-
-        for s in self.students:
-            self.data.append({self.head[0]: s.getId(), 
-                self.head[1]: s.getName(), 
-                self.head[2]: s.getGrade("midterm"), 
-                self.head[3]: s.getGrade("final"), 
-                self.head[4]: s.getGrade("ass1"),
-                self.head[5]: s.getGrade("ass2"),
-                self.head[6]: s.getGrade("ass3"),
-                self.head[7]: s.getTeamName(), 
-                self.head[8]: s.getGrade("teamproject")})
-
+            CS22B.students.append(Student(line[0], line[1], int(line[2]), int(line[3]), int(line[4]), int(line[5]), int(line[6]), line[7], int(line[8])))
+            
     def showAllData(self):
-        self.rewrite()
-        for i in self.data:
-            print(i)
+        for s in Student.students:
+            print(self.head[0],":",s.getId(),end=", ")
+            print(self.head[1],":",s.getName(),end=", ")
+            print(self.head[2],":",s.getGrade("midterm"),end=", ")
+            print(self.head[3],":",s.getGrade("final"),end=", ")
+            print(self.head[4],":",s.getGrade("ass1"),end=", ")
+            print(self.head[5],":",s.getGrade("ass2"),end=", ")
+            print(self.head[6],":",s.getGrade("ass3"),end=", ")
+            print(self.head[7],":",s.getName(),end=", ")
+            print(self.head[8],":",s.getGrade("teamproject"),end="\n")
     
     def showSomeData(self, column_name):
-        self.rewrite()
-
         if column_name == "midterm" or column_name == "final" or "ass" in column_name or column_name == "teamproject":
             for student in self.students:
                 print(student.getName() + ":",student.getGrade(column_name), end="\n")
@@ -44,6 +31,9 @@ class CS22B:
         elif column_name == "teamname":
             for student in self.students:
                 print(student.getName() +" is in "+ student.getTeamName(), end="\n")
+        elif column_name == "name":
+            for student in self.students:
+                print(student.getName())
         else:
             print("No such column name like that.",end="\n")
     
@@ -78,36 +68,33 @@ class CS22B:
 class Grade:
     full_grade = 0
 
-    def __init__(self, title, grade):
-        self.title = title
+    def __init__(self, grade):
         self.grade = grade
 
-    def getGrade(self):
+    def getAGrade(self):
         return self.grade
 
-    def setGrade(self, new_grade):
+    def setAGrade(self, new_grade):
         self.grade = new_grade
 
     def getFullGrade(self):
-        if self.full_grade == 0:
+        if Grade.full_grade == 0:
             return int(input("You have not set full_grade. Enter the full grade. "))
-        return self.full_grade
+        return Grade.full_grade
 
 class Student:
-    grades = {}
-    assignments = []
     def __init__(self,id,name, midterm, final, ass1, ass2, ass3, team_name,teamproject_grade):
+        print("I got",id,name,midterm,final,ass1,ass2,ass3,team_name,teamproject_grade)
         self.name = name
         self.id = id
-        self.grades["midterm"] = Grade("midterm", midterm)
-        self.grades["final"] = Grade("final",final)
-        self.grades["ass1"] = Grade("ass1",ass1)
-        self.grades["ass2"] = Grade("ass2",ass2)
-        self.grades["ass3"] = Grade("ass3",ass3)
-        self.teamproject = TeamProject(team_name)
-        self.grades["teamproject"] = Grade("teampproject",teamproject_grade)
+        self.grades = {}
+        self.grades["midterm"] = Exam("midterm", midterm)
+        self.grades["final"] = Exam("final",final)
+        self.grades["ass1"] = Assignment("ass1",ass1)
+        self.grades["ass2"] = Assignment("ass2",ass2)
+        self.grades["ass3"] = Assignment("ass3",ass3)
+        self.grades["teamproject"] = TeamProject(team_name, teamproject_grade)
 
-    
     def getId(self):
         return self.id
 
@@ -115,52 +102,43 @@ class Student:
         return self.name
 
     def getGrade(self, title):
-        return self.grades[title].getGrade()
+        return self.grades[title].getAGrade()
 
     def setGrade(self, title, new_grade):
-        self.grades[title].setGrade(new_grade)
+        self.grades[title].setAGrade(new_grade)
     
     def getTeamName(self):
-        return self.teamproject.getTeamName()
+        return self.grades["teamproject"].getTeamName()
     
     def getAllGrades(self):
-        return self.grades
+        return {i: self.grades[i] for i in self.grades.keys()}
 
     def getFullGrade(self, title):
         return self.grades[title].getFullGrade()
-    
-class Assignment:
-    def __init__(self, title):
+
+
+class Assignment(Grade):
+    def __init__(self, title, grade):
         self.title = title
+        super().__init__(grade)
 
     def getTitle(self):
         return self.title
 
 
-class TeamProject:
-    def __init__(self, team_name):
+class TeamProject(Grade):
+    def __init__(self, team_name, grade):
         self.team_name = team_name
+        super().__init__(grade)
 
     def getTeamName(self):
         return self.team_name
 
 
 class Exam(Grade):
-    full_grade = 0
-
     def __init__(self, exam_type, grade):
         self.kind = exam_type
-        self.grade = grade
-
-    def isFullGradeSet(self):
-        return True if self.full_grade != 0 else False
-
-    def setFullGrade(self, full_grade):
-        self.full_grade = full_grade
-
-    def getFullGrade(self):
-        return self.full_grade
-
+        super().__init__(grade)
 
 def getData(filename):
     try:
